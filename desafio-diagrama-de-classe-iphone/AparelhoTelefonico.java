b834440484c2ad6825711599168357308b186c88
Lio20 <31165033+Lio20@users.noreply.github.com>
2024-08-19 21:53:04 -0300

    Desafio Diagrama De Classe IPHONE

---
+++ desafio-diagrama-de-classe-iphone/AparelhoTelefonico.java
@@ -0,0 +1,8 @@
+package src;
+
+
+public interface AparelhoTelefonico {
+    public void ligar(String numero);
+    public void atender(String numero);
+    public void iniciarCorreioVoz(String mensagem, String destinatario);
+}
